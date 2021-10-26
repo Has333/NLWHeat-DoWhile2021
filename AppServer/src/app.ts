53c9d43d829c22c1d249bea041de5e61c01410b5
William Martins <williammds00@gmail.com>
2021-10-26 18:36:22 -0300

    feat: add and integrate frontend with backend

---
+++ AppServer/src/app.ts
@@ -0,0 +1,42 @@
+import "dotenv/config";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
+
+import { router } from "./routes";
+
+const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", socket => {
+    console.log(`Usuário conectado no socket ${socket.id}`);
+});
+
+app.use(express.json());
+
+app.use(router);
+
+app.get("/github", (request, response) => {
+    const githubRedirectPath = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`;
+
+    console.log(githubRedirectPath)
+
+    response.redirect(githubRedirectPath)
+})
+
+app.get("/signin/callback", (request, response) => {
+    const { code } = request.query;
+
+    return response.json(code);
+})
+
+export { serverHttp, io };
